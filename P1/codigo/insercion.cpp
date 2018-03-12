7e5e7987bec4c224f3d361fb673a31a78e998326
Fran González <fran.98.gg@gmail.com>
2018-03-12 16:02:28 +0100

    salidas insercion y seleccion

--- P1/codigo/insercion.cpp
+++ P1/codigo/insercion.cpp
@@ -1,33 +1,34 @@
 /**
-   @file Ordenación por inserción
+   @file Ordenaciï¿½n por inserciï¿½n
 */
 
-   
 #include <iostream>
-using namespace std;
 #include <ctime>
 #include <cstdlib>
 #include <climits>
 #include <cassert>
+#include <chrono>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <fstream>
 
-
-
-
-
+using namespace std;
+using namespace std::chrono;
 
 /* ************************************************************ */ 
-/*  Método de ordenación por inserción  */
+/*  Mï¿½todo de ordenaciï¿½n por inserciï¿½n  */
 
 /**
-   @brief Ordena un vector por el método de inserción.
+   @brief Ordena un vector por el mï¿½todo de inserciï¿½n.
 
    @param T: vector de elementos. Debe tener num_elem elementos.
              Es MODIFICADO.
-   @param num_elem: número de elementos. num_elem > 0.
+   @param num_elem: nï¿½mero de elementos. num_elem > 0.
 
    Cambia el orden de los elementos de T de forma que los dispone
    en sentido creciente de menor a mayor.
-   Aplica el algoritmo de inserción.
+   Aplica el algoritmo de inserciï¿½n.
 */
 inline static 
 void insercion(int T[], int num_elem);
@@ -35,27 +36,27 @@ void insercion(int T[], int num_elem);
 
 
 /**
-   @brief Ordena parte de un vector por el método de inserción.
+   @brief Ordena parte de un vector por el mï¿½todo de inserciï¿½n.
 
-   @param T: vector de elementos. Tiene un número de elementos 
+   @param T: vector de elementos. Tiene un nï¿½mero de elementos 
                    mayor o igual a final. Es MODIFICADO.
-   @param inicial: Posición que marca el incio de la parte del
+   @param inicial: Posiciï¿½n que marca el incio de la parte del
                    vector a ordenar.
-   @param final: Posición detrás de la última de la parte del
+   @param final: Posiciï¿½n detrï¿½s de la ï¿½ltima de la parte del
                    vector a ordenar. 
 		   inicial < final.
 
    Cambia el orden de los elementos de T entre las posiciones
    inicial y final - 1de forma que los dispone en sentido creciente
    de menor a mayor.
-   Aplica el algoritmo de inserción.
+   Aplica el algoritmo de inserciï¿½n.
 */
 static void insercion_lims(int T[], int inicial, int final);
 
 
 
 /**
-   Implementación de las funciones
+   Implementaciï¿½n de las funciones
 **/
 
 inline static void insercion(int T[], int num_elem)
@@ -79,33 +80,57 @@ static void insercion_lims(int T[], int inicial, int final)
   };
 }
 
-
-
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  
-    if (argc != 2)
-    {
-      cerr << "Formato " << argv[0] << " <num_elem>" << endl;
-      return -1;
-    }
+  const unsigned GAP = 12;
+  ofstream salida("salidas/insercion.dat", ios::app);
+  if (!salida)
+  {
+    cerr << "Error al abrir el fichero de salida";
+    exit(1);
+  }
+
+  if (argc != 2)
+  {
+    cerr << "Formato " << argv[0] << " <num_elem>" << endl;
+    return -1;
+  }
 
   int n = atoi(argv[1]);
 
-  int * T = new int[n];
+  int *T = new int[n];
   assert(T);
 
   srandom(time(0));
 
   for (int i = 0; i < n; i++)
-    {
-      T[i] = random();
-    };
+  {
+    T[i] = random();
+  }
+  std::vector<duration<double, std::milli>> score(100);
 
-  insercion(T, n);
+  for (auto &s : score)
+  {
+    auto start = high_resolution_clock::now();
+    insercion(T, n);
+    auto stop = high_resolution_clock::now();
 
-  delete [] T;
+    s = stop - start;
+  }
+
+  std::nth_element(score.begin(),
+                   score.begin() + score.size() / 2,
+                   score.end());
+  for (unsigned i = 0; i < n; i++)
+    cout << T[i];
+
+  salida << std::setw(GAP) << n
+         // << std::setw(GAP) << std::fixed << std::setprecision(1)
+         << std::setw(GAP) << score[score.size() / 2].count()
+         << std::endl;
+
+  delete[] T;
+  salida.close();
 
   return 0;
 };
- 
--- P1/scripts/script-n2.sh
+++ P1/scripts/script-n2.sh
@@ -2,5 +2,5 @@
 
 for ((N=100;N<10000;N=N+300))
 do
-  bin/burbujaO2 $N
+  bin/insercion $N
 done
